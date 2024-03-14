c73cbe11fed3ddad167d7806c4acb7c4b599785e
Darukio <damian-200@live.com.ar>
2024-03-14 12:55:17 -0300

    Configuración de inicio del servicio

--- configure_startup.py
+++ configure_startup.py
@@ -8,31 +8,37 @@ def create_service():
     venv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "venv")
     venv_python = os.path.join(venv_path, "Scripts", "python.exe") if sys.platform == "win32" else os.path.join(venv_path, "bin", "python")
 
-    # Obtener la ruta completa del script main.py
-    main_script_path = os.path.abspath("main.py")
+    # Obtener la ruta completa del script service.py
+    main_script_path = os.path.abspath("service.py")
 
-    # Crear el servicio de Windows
+    # Instalar el servicio de Windows
     create_service_cmd = [venv_python, main_script_path, "install"]
     subprocess.run(create_service_cmd, check=True)
 
-def configure_startup_exe():
+def configure_startup():
     # Obtener la ruta completa del ejecutable .exe
     exe_path = os.path.join("dist", "main.exe")
-
+    key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+    reg_key = reg.OpenKey(reg.HKEY_CURRENT_USER, key, 0, reg.KEY_SET_VALUE)
     if os.path.exists(exe_path):
         # Agregar el script como servicio de Windows
-        key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
         try:
-            reg_key = reg.OpenKey(reg.HKEY_CURRENT_USER, key, 0, reg.KEY_SET_VALUE)
             reg.SetValueEx(reg_key, "EmsaRelojService", 0, reg.REG_SZ, exe_path)
-            reg.CloseKey(reg_key)
-            print("Configuración de inicio con Windows realizada correctamente.")
         except Exception as e:
             print(f"Error al configurar el inicio con Windows: {e}")
+    else:
+        # Agregar el script Python como servicio de Windows
+        pyScriptPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "main.py")
+        try:
+            reg.SetValueEx(reg_key, "EmsaRelojService", 0, reg.REG_SZ, f'"{sys.executable}" "{pyScriptPath}"')
+        except Exception as e:
+            print(f"Error al configurar el inicio con Windows: {e}")
+    reg.CloseKey(reg_key)
+    print("Configuración de inicio con Windows realizada correctamente.")
 
 if __name__ == "__main__":
     # Crear el servicio
     create_service()
 
     # Configurar el inicio con Windows
-    configure_startup_exe()
+    # configure_startup()
