60afa81208861196a841a60892992a7b3feec9f7
Darukio <damian-200@live.com.ar>
2024-04-23 10:01:57 -0300

    validate hour

--- connection.py
+++ connection.py
@@ -1,7 +1,6 @@
 from zk import ZK
 from datetime import datetime
 from utils import logging
-import threading
 
 def conectar(ip, port):
     conn = None
@@ -10,36 +9,15 @@ def conectar(ip, port):
         logging.info(f'Connecting to device {ip}...')
         conn = zk.connect()
     except Exception as e:
-        logging.warning(e)
         # Si falla la conexión, iniciar un hilo aparte para intentar nuevamente
-        thread = threading.Thread(target=reintentar_conexion, args=(ip, port))
-        thread.start()
-        pass
-    finally:
-        if conn is not None:
-            #logging.info('Disabling device...')
-            #conn.disable_device()
-            logging.info(f'Successfully connected to device {ip}.')
-            conn.test_voice(index=10)
-        return conn
-
-def reintentar_conexion(ip, port):
-    logging.info(f'Retrying connection to device {ip}...')
-    intentos_maximos = 3
-    intentos = 0
-    while intentos < intentos_maximos:  # Intenta conectar hasta 3 veces
-        try:
-            zk = ZK(ip, port, timeout=5)
-            logging.info(f'Successfully connected to device {ip}.')
-            conn = zk.connect()
-            conn.test_voice(index=10)
-            return conn
-        except Exception as e:
-            logging.warning(f'Failed to connect to device {ip}. Retrying...')
-            intentos += 1
-    logging.error(f'Unable to connect to device {ip} after {intentos} attempts.')
-    raise Exception
-
+        raise Exception(str(e))
+    if conn is not None:
+        #logging.info('Disabling device...')
+        #conn.disable_device()
+        logging.info(f'Successfully connected to device {ip}.')
+        conn.test_voice(index=10)
+    return conn
+    
 def finalizar_conexion(conn):
     #logging.info('Enabling device...')
     #conn.enable_device()
@@ -51,22 +29,28 @@ def actualizar_hora(conn):
     try:
         validar_hora(conn.get_time())
     except Exception as e:
-        raise Exception(e)
+        raise Exception(str(e))
     # update new time to machine
     newtime = datetime.today()
     conn.set_time(newtime)
 
 def validar_hora(zktime):
     newtime = datetime.today()
-    logging.debug(f'Hour device: {zktime.hour}:{zktime.min} - Hour machine: {newtime.hour}:{newtime.min}')
-    if zktime.hour != newtime.hour or zktime.min != newtime.min:
-        raise Exception('Hours between device and machine doesn\'t match')
+    logging.info(f'Date and hour device: {zktime} - Date and hour machine: {newtime}')
+    logging.debug(f'Device: {zktime.day}/{zktime.month}/{zktime.year} - Machine: {newtime.day}/{newtime.month}/{newtime.year}')
+    if (abs(zktime.hour - newtime.hour) > 0 or
+    abs(zktime.minute - newtime.minute) >= 5 or
+    zktime.day != newtime.day or
+    zktime.month != newtime.month or
+    zktime.year != newtime.year):
+        raise Exception('Hours or date between device and machine doesn\'t match')
     
 def obtener_marcaciones(conn, intentos=0):
     attendances = []
     try:
         logging.info('Getting attendances...')
         attendances = conn.get_attendance()
+        logging.info('Disabling device OFF')
         #conn.clear_attendance()
         logging.debug(f'Length of attendances from device: {conn.records}, Length of attendances: {len(attendances)}')
         if conn.records != len(attendances):
@@ -77,5 +61,4 @@ def obtener_marcaciones(conn, intentos=0):
                 logging.error("Failed to retrieve attendances after 3 attempts.")
     except Exception as e:
         logging.error(f'Process terminated: {e}')
-    finally:
-        return attendances
+    return attendances
