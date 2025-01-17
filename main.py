1c390f926ffbedf52136a1c12796c79e4f3d1d27
Darukio <damian-200@live.com.ar>
2025-01-17 13:08:25 -0300

    run admin, check and install service, verify duplicated instance

--- main.py
+++ main.py
@@ -19,21 +19,124 @@
 
 import eventlet
 eventlet.monkey_patch()
+from schedulerService import check_and_install_service
+import ctypes
+import subprocess
+from scripts.ui.message_box import MessageBox
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QApplication
 from scripts.ui.icon_manager import MainWindow
 from scripts.utils.logging import config_log, logging
 from scripts.utils.file_manager import encontrar_directorio_raiz
 import sys
 import os
-import configparser
-import threading
+import psutil
 
 # Versión del programa
-VERSION = "v1.1.4"
+VERSION = "v2.1.22"
 
 # Para leer un archivo INI
 from scripts import config
 config.read(os.path.join(encontrar_directorio_raiz(), 'config.ini'))
+
+def obtener_proceso_padre(pid):
+    """
+    Obtiene el proceso padre de un proceso dado.
+
+    Args:
+        pid (int): PID del proceso.
+
+    Returns:
+        psutil.Process: Objeto del proceso padre o None si no existe.
+    """
+    try:
+        proceso = psutil.Process(pid)
+        return proceso.parent()
+    except psutil.NoSuchProcess:
+        print(f"No existe el proceso con PID {pid}.")
+        return None
+    
+def obtener_procesos_hijos(pid):
+    """
+    Obtiene una lista de procesos hijos de un proceso dado.
+
+    Args:
+        pid (int): PID del proceso.
+
+    Returns:
+        list: Lista de objetos psutil.Process que representan los hijos.
+    """
+    try:
+        proceso = psutil.Process(pid)
+        return proceso.children()
+    except psutil.NoSuchProcess:
+        print(f"No existe el proceso con PID {pid}.")
+        return []
+
+def is_user_admin():
+    """
+    Verifica si el proceso tiene privilegios de administrador.
+    """
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except Exception as e:
+        print(f"Error al verificar privilegios: {e}")
+        return False
+
+def run_as_admin():
+    """
+    Relanza el script con privilegios de administrador si es necesario.
+    """
+    if not is_user_admin():
+        # El script no se está ejecutando como administrador, por lo tanto, lo reiniciamos con privilegios elevados
+        script = sys.argv[0]
+        params = " ".join(sys.argv[1:])
+
+        logging.debug("script: "+script
+            + " " + params)
+
+        # Ejecutar el script con permisos elevados
+        if script.endswith(".exe"):  # Si es un archivo .exe
+            # Re-ejecutar el script con permisos de administrador
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        else:  # Si es un script de Python
+            subprocess.run(['powershell', 'Start-Process', 'pythonw', '-ArgumentList', f'"{script}" {params}', '-Verb', 'RunAs'])
+        sys.exit(0)  # Finaliza el proceso original
+
+def verificar_instancia_duplicada(script_name):
+    # Obtener el nombre del script sin la ruta completa
+    script_basename = os.path.basename(script_name)
+    
+    # Iterar sobre todos los procesos activos
+    for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
+        try:
+            # Ignorar procesos de otros programas que no sean el script actual
+            if proc.info['name'] == 'python.exe' or proc.info['name'] == 'pythonw.exe':
+                # Verificamos si la instancia del script ya está corriendo
+                if script_basename in proc.info['cmdline']:
+                    if proc.info['pid'] != os.getpid():
+                        # Si encontramos otra instancia que no es la actual
+                        logging.info(f"Instancia duplicada encontrada: {proc.info['cmdline']}")
+                        MessageBox(QMessageBox.Warning, "Ya hay una instancia de la aplicación en ejecución.").exec_()
+                        return True
+            if proc.info['name'] == script_basename:
+                if (
+                    proc.info['pid'] != os.getpid()  # No es el proceso actual
+                    and proc.info['pid'] != obtener_proceso_padre(os.getpid()).pid  # No es el proceso padre
+                    and proc.info['pid'] not in [p.pid for p in obtener_procesos_hijos(os.getpid())]  # No es un proceso hijo
+                ):
+                    # Si encontramos otra instancia que no es la actual ni está relacionada
+                    logging.info(f"Instancia duplicada encontrada: {proc.info['cmdline']}")
+                    return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    
+    # Si no encontramos una instancia duplicada, devolvemos False
+    return False
+
+def cerrar_instancia_duplicada():
+    print("Cerrando instancia duplicada...")
+    sys.exit(0)  # Salir del script si se encuentra duplicado
 
 def main():
     config_log()
@@ -41,6 +144,23 @@ def main():
     import os
     logging.debug('Script executing...')
 
+    # Nombre del script actual
+    script_name = sys.argv[0]
+
+    logging.debug(script_name)
+    
+    logging.debug(os.getcwd())
+    logging.debug(f'ADMIN: {is_user_admin()}')
+
+    if not is_user_admin():
+        run_as_admin()
+
+    # Verificar si ya está ejecutándose una instancia duplicada
+    if verificar_instancia_duplicada(script_name):
+        cerrar_instancia_duplicada()
+
+    check_and_install_service()
+        
     MODE = 'User' if getattr(sys, 'frozen', False) else 'Developer'
     msg_init = f"Program version: {VERSION} - Mode: {MODE}"
     logging.info(msg_init)
@@ -49,15 +169,13 @@ def main():
 
     config_log_console()
     config_content()
-
-    if len(sys.argv) == 1:
-        try:
-            app = QApplication(sys.argv)
-            main_window = MainWindow()
-            sys.exit(app.exec_())
-            
-        except Exception as e:
-            logging.error(e)
+    logging.debug(sys.argv)
+    try:
+        app = QApplication(sys.argv)
+        main_window = MainWindow()
+        sys.exit(app.exec_())
+    except Exception as e:
+        logging.critical(e)
 
 def config_content():
     for section in config.sections():
